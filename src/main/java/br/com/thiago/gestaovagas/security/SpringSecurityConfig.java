8f7ab37518ba1a362f436e5383a5e060069f0780
Thiago Rasquino <thiago.programador.net@gmail.com>
2024-07-16 19:02:21 -0300

    Adicionando bean de encrypt para senhas

--- src/main/java/br/com/thiago/gestaovagas/security/SpringSecurityConfig.java
+++ src/main/java/br/com/thiago/gestaovagas/security/SpringSecurityConfig.java
@@ -3,6 +3,8 @@ package br.com.thiago.gestaovagas.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +12,17 @@ public class SpringSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/candidate").permitAll();
+                    auth.requestMatchers("/company").permitAll();
+                    auth.anyRequest().authenticated();
+                });
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
