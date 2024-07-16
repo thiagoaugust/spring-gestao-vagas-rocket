0c12f336c6cdb3fc6070738e01a0e18d3a089d73
Thiago Rasquino <thiago.programador.net@gmail.com>
2024-07-16 19:23:57 -0300

    Finalização do controller de token

--- src/main/java/br/com/thiago/gestaovagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/thiago/gestaovagas/modules/company/controllers/AuthCompanyController.java
@@ -3,6 +3,8 @@ package br.com.thiago.gestaovagas.modules.company.controllers;
 import br.com.thiago.gestaovagas.modules.company.dto.AuthCompanyDto;
 import br.com.thiago.gestaovagas.modules.company.usecases.AuthCompanyUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,13 @@ public class AuthCompanyController {
     private AuthCompanyUseCase authCompanyUseCase;
 
     @PostMapping("/company")
-    public String create(@RequestBody AuthCompanyDto authCompanyDto) throws AuthenticationException {
-        return this.authCompanyUseCase.execute(authCompanyDto);
+    public ResponseEntity<Object> create(@RequestBody AuthCompanyDto authCompanyDto) {
+        try {
+            String result = this.authCompanyUseCase.execute(authCompanyDto);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
     }
 
 }
